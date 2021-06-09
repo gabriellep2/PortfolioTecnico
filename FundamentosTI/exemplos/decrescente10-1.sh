6890ef9737c9820fd75556f9670d394b6ab4cca3
gabriellep2 <83764942+gabriellep2@users.noreply.github.com>
2021-06-09 17:34:54 -0300

    Update decrescente10-1.sh

--- FundamentosTI/exemplos/decrescente10-1.sh
+++ FundamentosTI/exemplos/decrescente10-1.sh
@@ -2,6 +2,8 @@ localhost:~# vi decrescente10-1.sh
 localhost:~# chmod u+x decrescente10-1.sh
 localhost:~# cat decrescente10-1.sh
 #!/bin/bash
+# Autora: Gabrielle Pires
+# Descrição: Esse arquivo irá imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem decrescente
 i=10
 while [ $i -ge 1 ]
 do
