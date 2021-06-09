6bbb1177a58aeb14fd22ffcc506e97433a48e49c
gabriellep2 <83764942+gabriellep2@users.noreply.github.com>
2021-06-09 17:25:14 -0300

    Create crescente1-10.sh

---
+++ FundamentosTI/exemplos/crescente1-10.sh
@@ -0,0 +1,22 @@
+localhost:~#vi crescente1-10.sh
+localhost:~# chmod u+x crescente1-10.sh
+localhost:~# cat crescente1-10.sh
+#!/bin/bash
+i=1
+while [ $i -le 10 ]
+do
+    echo "- $i"
+    i=$((i+1))
+done
+ 
+localhost:~# ./crescente1-10.sh
+- 1
+- 2
+- 3
+- 4
+- 5
+- 6
+- 7
+- 8
+- 9
+- 10
